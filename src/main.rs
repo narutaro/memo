1358b1634629bfe6029fecec077b8530ed4d254a
narutaro <narusan@mac.com>
2021-08-08 10:05:10 +0900

    in-memory list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use structopt::StructOpt;
+
+#[derive(Debug)]
+struct Memo {
+    id: isize,
+    name: String,
+    pri: isize,
+}
+
+#[derive(StructOpt, Debug)]
+/// Help you take notes
+enum Opt {
+    /// add a memo
+    Add { name: String },
+    /// delete a memo
+    Del { id: String },
+    /// list memos
+    List {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let memo1 = Memo {
+        id: 1,
+        name: String::from("my first memo"),
+        pri: 1,
+    };
+    let memo2 = Memo {
+        id: 2,
+        name: String::from("my second memo"),
+        pri: 1,
+    };
+    let memo3 = Memo {
+        id: 3,
+        name: String::from("my third memo"),
+        pri: 1,
+    };
+
+    let mut memos: Vec<Memo> = Vec::new();
+    memos.push(memo1);
+    memos.push(memo2);
+    memos.push(memo3);
+
+    match Opt::from_args() {
+        Opt::Add { name } => println!("{} added", name),
+        Opt::Del { id } => println!("{} deleted", id),
+        Opt::List {} => println!("{:#?}", memos),
+    };
 }
