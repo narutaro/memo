195b89a596bab8287adeb763b41b368559bbc448
narutaro <narusan@mac.com>
2021-08-15 17:35:45 +0900

    just put stuff needed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate simplebase;
+use serde::{Deserialize, Serialize};
+use simplebase::engine::*;
 use structopt::StructOpt;
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Memo {
     id: isize,
     body: String,
@@ -18,7 +21,47 @@ enum Opt {
     List {},
 }
 
+// DB
+fn list(db: &RecordData) {
+    println!("----");
+    for (k, v) in &db.hash_data {
+        println!("k: {:?}, v: {:?}", k, v);
+    }
+}
+
 fn main() {
+    //
+    // Serde
+    //
+    let memo = Memo {
+        id: 1,
+        body: "first memo".to_string(),
+        star: true,
+    };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&memo).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Memo = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+
+    //
+    // DB
+    //
+    let mut db = new_empty_database();
+    db.add_record(serialized);
+    db.save_database("memo.db");
+    list(&db);
+
+    //
+    // CLI
+    //
     let memo1 = Memo {
         id: 1,
         body: String::from("my first memo"),
